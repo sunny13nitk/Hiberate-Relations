bce251c4958f4f2418cfd3779083182f34e529a8
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-05-21 12:54:59 +0530

    commit

---
+++ src/root/demo/CreateCourse_Reviews.java
@@ -0,0 +1,62 @@
+package root.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import root.POJOs.Course;
+import root.POJOs.Instructor;
+import root.POJOs.Reviews;
+
+public class CreateCourse_Reviews
+{
+	
+	public static void main(
+	        String[] args
+	)
+	{
+		// Create Session Factory
+		SessionFactory sFac = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Reviews.class)
+		        .addAnnotatedClass(Course.class).addAnnotatedClass(Instructor.class).buildSessionFactory();
+		
+		// Get Session Instance from Factory
+		
+		Session sess = sFac.getCurrentSession();
+		
+		try
+		{
+			
+			// Begin Transaction from Session
+			
+			sess.beginTransaction();
+			
+			int courseId = 1;
+			
+			// Do action as per Transaction - Save Here
+			Course course = sess.get(Course.class, courseId);
+			if (course != null)
+			{
+				Reviews Review1 = new Reviews("Nice Course on Promoters Integrity");
+				Reviews Review2 = new Reviews("Nice Course on Cash Flow Analysis");
+				course.addReview(Review1);
+				course.addReview(Review2);
+				sess.save(Review1);
+				sess.save(Review2);
+			}
+			
+			// Commit the Txn
+			sess.getTransaction().commit();
+			
+		} catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally // Close the Session Factory
+		{
+			sess.close();
+			sFac.close();
+		}
+		
+	}
+	
+}
